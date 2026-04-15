582d232b753bd55d50297dd0baab80fa9593096a
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-04-15 17:30:49 +0100

    fire drill two

---
+++  fire drill two/TaskOne.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+      public class TaskOne {
+        public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); 
+                start = 0
+     for (int i = 1; i<=10; i++) {
+            System.out.println("Enter First number: ");
+                   int numberone = input.nextInt();
+
+        
+
+
+
+
+
+
