a9a83eff8764e4012ada8c06b5aad3de38f2e73d
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-04-15 17:06:22 +0100

    Fire Drill Two

---
+++ Fire Drill Two/TaskFour.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+      public class TaskFour {
+        public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); 
+     
+            System.out.println("Enter First number: ");
+                   int numberone = input.nextInt();
+
+        System.out.println("Enter second number: ");
+                int numbertwo = input.nextInt();
+
+             System.out.println("Enter third number: ");
+                int numberthree = input.nextInt();
+
+                         System.out.println("Enter fourth number: ");
+                int numberfour = input.nextInt();
+
+                         System.out.println("Enter fifth number: ");
+                int numberfive = input.nextInt();
+
+                     System.out.println("Enter sixth number: ");
+                int numbersix = input.nextInt();
+
+                         System.out.println("Enter seventh number: ");
+                int numberseven = input.nextInt();
+
+                             System.out.println("Enter eighth number: ");
+                int numbereight = input.nextInt();
+                            
+                                     System.out.println("Enter ninth number: ");
+                int numbernine = input.nextInt();
+                             System.out.println("Enter tenth number: ");
+                int numberten= input.nextInt();
+
+
+          if (numbertwo % 2 == 0) {
+
+        System.out.println(sum of )
+
+
+
+}
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ Fire Drill Two/TaskThree.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+      public class TaskThree {
+        public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in); 
+     
+            System.out.println("Enter First number: ");
+                   int numberone = input.nextInt();
+
+        System.out.println("Enter second number: ");
+                int numbertwo = input.nextInt();
+
+             System.out.println("Enter third number: ");
+                int numberthree = input.nextInt();
+
+                         System.out.println("Enter fourth number: ");
+                int numberfour = input.nextInt();
+
+                         System.out.println("Enter fifth number: ");
+                int numberfive = input.nextInt();
+
+                     System.out.println("Enter sixth number: ");
+                int numbersix = input.nextInt();
+
+                         System.out.println("Enter seventh number: ");
+                int numberseven = input.nextInt();
+
+                             System.out.println("Enter eighth number: ");
+                int numbereight = input.nextInt();
+                            
+                                     System.out.println("Enter ninth number: ");
+                int numbernine = input.nextInt();
+                             System.out.println("Enter tenth number: ");
+                int numberten= input.nextInt();
+int average = ((numberone + numbertwo + numberthree + numberfour + numberfive + numbersix + numberseven + numbereight + numbernine + numberten)/ 10);
+ int sum = (numberone + numbertwo + numberthree + numberfour + numberfive + numbersix + numberseven + numbereight + numbernine + numberten);                   
+
+
+
+                        System.out.println("Average: " + average);
+                        System.out.println("Sum: " + sum);
+                            
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
