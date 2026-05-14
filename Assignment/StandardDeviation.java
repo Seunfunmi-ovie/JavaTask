e83fa7a5d4aa9a665b977a312f43eeb9c7dda0e4
oluwaseunpu@gmail.com <kpomassiurban@gmail.com>
2026-05-14 08:13:09 +0100

    Update Assignment with test

--- Assignment/StandardDeviation.java
+++ Assignment/StandardDeviation.java
@@ -2,29 +2,43 @@ public class StandardDeviation {
 
     public static void main(String [] args){
 
-   double [] numbers ={3,4,5,2,1,6,7};
-    System.out.print(calculateStandardDeviation(numbers));
+          double[] numbers = {3, 4, 5, 2, 1, 6, 7};
 
+        double sum = calculateSum(numbers);
+        double mean = calculateMean(sum,numbers);
+        double standardDeviation = calculateStandardDeviation(numbers, mean);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Mean: " + mean);
+        System.out.println("Standard Deviation: " + standardDeviation);
     }
     
     
-    public static double calculateStandardDeviation(double [] number){
-   double sum = 0;
-    for (int count = 1; count < number.length; count++){
-    sum += number[count];
-    }
-    double mean = sum/number.length;
-    double square = 0;
-    for (int count = 0; count < number.length; count++){
-    double num = number[count] - mean;
-    square += num * num;
-    }
-    double variance = square/(number.length - 1);
-    double standard = Math.sqrt(variance);
+            public static double calculateSum(double [] number){
+                 double sum = 0;
+                for (int count = 1; count < number.length; count++){
+                     sum += number[count];
+                 }
+                 return sum;
+            }
+            
+            public static double calculateMean(double sum, double[] number) {
+                return sum / number.length;
+            }
     
     
-   return standard;
-    }
+           public static double calculateStandardDeviation(double[] number, double mean) {
+                double squareSum = 0;
+                
+                for (int count = 0; count < number.length; count++) {
+                    double num = number[count] - mean;
+                    squareSum += num * num;
+                }
+
+
+                double variance = squareSum / (number.length - 1);
+                return Math.sqrt(variance);
+            }
    
     }
    
--- Assignment/StandardDeviationTest.java
+++ Assignment/StandardDeviationTest.java
@@ -8,8 +8,38 @@ public class StandardDeviationTest{
         public void testThatFunctionShouldAddSumOfNumbers(){
         
         double [] numbers = {3,4,5,2,1,6,7};
-        double expected = 2.2092877;
-        double actual = StandardDeviation.calculateStandardDeviation(numbers);
+        double expected = 25.0;
+        double actual = StandardDeviation.calculateSum(numbers);
+        
+              
+        assertEquals(expected,actual);
+    
+        
+        }
+        
+        
+         @Test
+        public void testThatFunctionShouldAddSumOfNumbersEqualsMean(){
+        
+        double [] numbers = {3,4,5,2,1,6,7};
+        double sum = 25.0;
+        double expected = 3.5714285714285716;
+        double actual = StandardDeviation.calculateMean(sum, numbers);
+        
+              
+        assertEquals(expected,actual);
+    
+        
+        }
+        
+        @Test
+        public void testThatFunctionShouldAddSquareOfMeanEqualsStandardDeviation(){
+        
+        double [] numbers = {3,4,5,2,1,6,7};
+ 
+        double mean = 3.5714285714285716;
+        double expected = 0.5102040816326531;
+        double actual = StandardDeviation.calculateMean(mean, numbers);
         
               
         assertEquals(expected,actual);
